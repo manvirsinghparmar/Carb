f27db923ef1ed57ad381bc7c333b32c3e0803821
Shaminderca <56711365+Shaminderca@users.noreply.github.com>
2020-10-03 20:51:42 -0400

    Class by the name shaminder added (#1)

---
+++ src/main/java/com/carbonite/qa/webpages/Shaminder.java
@@ -0,0 +1,10 @@
+package com.carbonite.qa.webpages;
+
+import com.carbonite.qa.base.TestBase;
+
+public class Shaminder extends TestBase{
+	public static void main(String args[])
+	{
+		System.out.println("Changes in Shaminder Branch");
+	}
+}
