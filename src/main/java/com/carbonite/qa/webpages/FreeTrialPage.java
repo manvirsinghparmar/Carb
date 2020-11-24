49d4c14f56ab380c17687a43358c9c5df4a3b352
Manvir <manvir.singh.parmar@gmail.com>
2020-11-24 12:10:01 -0500

    Sleep added

--- src/main/java/com/carbonite/qa/webpages/FreeTrialPage.java
+++ src/main/java/com/carbonite/qa/webpages/FreeTrialPage.java
@@ -12,6 +12,13 @@ public class FreeTrialPage extends TestBase {
 	}
 
 	public String getTitleOfThePage() {
+		
+		try {
+			Thread.sleep(10000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 		String titleOfThePage = driver.getTitle();
 
