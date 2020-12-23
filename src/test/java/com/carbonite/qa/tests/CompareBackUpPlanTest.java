b90297d008efcbd95c931d0e37060d8aaf818439
Manvir <manvir.singh.parmar@gmail.com>
2020-12-23 13:30:35 -0500

    Test disabled for compare backup plan page

--- src/test/java/com/carbonite/qa/tests/CompareBackUpPlanTest.java
+++ src/test/java/com/carbonite/qa/tests/CompareBackUpPlanTest.java
@@ -1,13 +1,11 @@
 package com.carbonite.qa.tests;
 
-import org.openqa.selenium.support.PageFactory;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import com.carbonite.qa.base.TestBase;
-import com.carbonite.qa.webpages.BuisnessSolutionPage;
 import com.carbonite.qa.webpages.CompareBackUpPlanPage;
 import com.carbonite.qa.webpages.SignUpPage;
 
@@ -28,7 +26,7 @@ public class CompareBackUpPlanTest extends TestBase {
 	}
 
 	// Verify the title of the page upon navigation
-	@Test(priority = 1)
+	@Test(priority = 1,enabled = false)
 	void getTitleOfThePage() {
 
 	
