2e6e80d06580176f95de276617d2b0bece4441a9
Manvir <manvir.singh.parmar@gmail.com>
2020-12-26 01:11:13 -0500

    Backup Plan page Updated

--- src/main/java/com/carbonite/qa/webpages/CompareBackUpPlanPage.java
+++ src/main/java/com/carbonite/qa/webpages/CompareBackUpPlanPage.java
@@ -1,10 +1,17 @@
 package com.carbonite.qa.webpages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 import com.carbonite.qa.base.TestBase;
 
 public class CompareBackUpPlanPage extends TestBase {
+	
+	
+	@FindBy(css="div>button")
+	public
+	WebElement buynow;
 
 	public CompareBackUpPlanPage() {
 		PageFactory.initElements(driver, this);
@@ -23,5 +30,6 @@ public class CompareBackUpPlanPage extends TestBase {
 
 		return titleOfThePage;
 	}
+	
 
 }
